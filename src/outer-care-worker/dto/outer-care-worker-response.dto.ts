d1e3da7ee2ea83498e73df65af8d14492598b45d
bjg <birdy_birdy@naver.com>
2021-05-16 16:39:05 +0900

    fix: 매칭제안서 목록 외부요양보호사 response 수정

--- src/outer-care-worker/dto/outer-care-worker-response.dto.ts
+++ src/outer-care-worker/dto/outer-care-worker-response.dto.ts
@@ -3,10 +3,12 @@ import { OuterCareWorkerEntity } from '../entity/outer-care-worker.entity';
 export default class OuterCareWorkerResponse {
   constructor(o: OuterCareWorkerEntity) {
     this.id = o.id;
-    this.name = o.name;
+    this.name = o.name ? o.name[0] + 'XX' : '';
     this.gender = o.isFemale ? '여성' : '남성';
     this.profile = o.profile;
     this.age = o.birthDay ? new Date().getFullYear() - new Date(o.birthDay).getFullYear() + 1 : 0;
+    this.address = o.address;
+    this.description = o.description;
   }
 
   public id: string;
@@ -14,4 +16,6 @@ export default class OuterCareWorkerResponse {
   public gender: string;
   public profile: string;
   public age: number;
+  public address: string;
+  public description: string;
 }
